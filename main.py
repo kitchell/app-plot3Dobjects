2d2d4ccdbc851972a61d5d5f13d6f8283a4f493a
Lindsey Kitchell <kitchell@indiana.edu>
2017-07-17 12:05:57 -0400

    update

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ view_up = [(0.05, 0.98, -0.21), (-0.02, -0.01, 1.00),
 views = ['axial', 'sagittal_left', 'coronal', 'sagittal_right']
 
 for file in glob.glob(config["surfaces"] + "/*.vtk"):
+    print file
 #for file in glob.glob('surfaces/*.vtk'):
     for d in range(len(camera_pos)):
         Render_3D(file, views[d], camera_pos[d], focal_point[d], view_up[d])
