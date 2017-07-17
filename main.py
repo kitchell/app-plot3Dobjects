37b8ccde4385cc523bcefb49b8ad92dab25f5ee3
Lindsey Kitchell <kitchell@indiana.edu>
2017-07-17 11:37:56 -0400

    added files

---
+++ main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 16 20:08:14 2017
+
+@author: lindseykitchell
+"""
+
+from render3D import Render_3D
+import json
+from xvfbwrapper import Xvfb
+import os
+import glob
+
+# start virtual display
+vdisplay = Xvfb()
+vdisplay.start()
+
+# read json file
+with open('config.json') as config_json:
+    config = json.load(config_json)
+
+pwd = os.getcwd()
+os.mkdir(pwd + "/images")
+
+camera_pos = [(-5.58, 84.98, 467.47), (-482.32, 3.58, -6.28),
+              (-58.32, 454.83, -14.22), (455.46, 9.14, 95.68)]
+focal_point = [(-8.92, -16.15, 4.47), (-8.92, -16.15, 4.47),
+               (-8.92, -16.15, 4.47), (-8.92, -16.15, 4.47)]
+view_up = [(0.05, 0.98, -0.21), (-0.02, -0.01, 1.00),
+           (-0.01, 0.04, 1.00), (-0.20, 0.21, 0.96)]
+views = ['axial', 'sagittal_left', 'coronal', 'sagittal_right']
+
+#for file in glob.glob(config["surfaces"] + "/*.vtk"):
+for file in glob.glob('surfaces/*.vtk'):
+    for d in range(len(camera_pos)):
+        Render_3D(file, views[d], camera_pos[d], focal_point[d], view_up[d])
+        
+
+vdisplay.stop()
---
+++ render3D.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 16 18:45:22 2017
+
+@author: lindseykitchell
+"""
+
+
+def Render_3D(file_name, view, camera_pos, focal_point, view_up):
+
+    # load VTK
+    #from vtk import *
+    
+    import vtk
+    from vtk.util.numpy_support import vtk_to_numpy
+    from dipy.viz import window, actor
+    
+    # Read the surface from file
+    object = vtk.vtkPolyDataReader()
+    object.SetFileName(file_name)
+    
+    objectMapper = vtk.vtkPolyDataMapper()
+    objectMapper.SetInputConnection(object.GetOutputPort())
+    objectMapper.ScalarVisibilityOff()
+    
+    objectActor=vtk.vtkActor()
+    objectActor.SetMapper(objectMapper)
+    objectActor.GetProperty().SetColor(0.5,0.5,0.5)
+    
+    # Attach to a renderer
+#    ren = vtk.vtkRenderer()
+#    ren.AddActor(objectActor)
+#    ren.SetBackground(0.1, 0.1, 0.1)
+    
+    # Attach to a window
+#    renWin = vtk.vtkRenderWindow()
+#    renWin.AddRenderer(ren)
+#    #renWin.SetWindowName("surface")
+#    renWin.SetSize(500,500)
+    
+    # Attach to an interactor
+#    iren = vtk.vtkRenderWindowInteractor()
+#    iren.SetRenderWindow(renWin)
+#    style = vtk.vtkInteractorStyleSwitch()
+#    style.SetCurrentStyleToTrackballCamera()
+#    iren.SetInteractorStyle(style)
+#    iren.Initialize()
+#    iren.Start()
+#    
+   
+    renderer = window.Renderer()
+    renderer.clear()
+    renderer.set_camera(position=camera_pos,
+                            focal_point=focal_point,
+                            view_up=view_up)
+    renderer.add(objectActor)
+    show_m = window.ShowManager(renderer, size=(800, 800))
+#    show_m.initialize()
+#    show_m.render()
+#    show_m.start()
+##     
+    fname = file_name[0:-4]+'_'+view+'.png'
+    window.snapshot(renderer, fname=fname, 
+                        size=(800, 800), offscreen=True, order_transparent=False)  
+    
