a8a06d86065ea33cc38a65e6708f1d5f393f2caf
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-07-19 12:04:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -56,10 +56,11 @@ color = {'Callosum_Forceps_Major_surf.vtk':[0.04850526316,0.6792842105,0.7341421
 
 for file in glob.glob(config["surfaces"] + "/*.vtk"):
     print file
-    if file in color.keys():
+    fname = os.path.basename(file_name)
+    if fname in color.keys():
 #for file in glob.glob('surfaces/*.vtk'):
         for d in range(len(camera_pos)):
-            Render_3D(file, views[d], camera_pos[d], focal_point[d], view_up[d], color[file])
+            Render_3D(file, views[d], camera_pos[d], focal_point[d], view_up[d], color[fname])
     else:
         image_color = [0.06577894737,0.4776315789,0.8531631579]
         for d in range(len(camera_pos)):
