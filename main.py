355c10a49fc2c9ac6a5906878f16c50cf99ec611
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-27 15:59:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import json
 from xvfbwrapper import Xvfb
 import os
 import glob
+import shutil
 
 # start virtual display
 vdisplay = Xvfb()
@@ -30,26 +31,26 @@ view_up = [(0.05, 0.98, -0.21), (-0.02, 0.01, -1.00),
            (-0.01, 0.04, -1.00), (-0.20, -0.21, -0.96)]
 views = ['axial', 'sagittal_left', 'coronal', 'sagittal_right']
 
-color = {'Callosum_Forceps_Major_surf.vtk':[0.04850526316,0.6792842105,0.7341421053],
-         'Callosum_Forceps_Minor_surf.vtk':[0.1400526316,0.7084789474,0.6680368421],
-         'Left_Arcuate_surf.vtk':[0.9595947368,0.8869315789,0.1189526316], 
-         'Left_Cingulum_Cingulate_surf.vtk': [0.06577894737,0.4776315789,0.8531631579], 
-         'Left_Cingulum_Hippocampus_surf.vtk':[0.03562105263,0.5946210526,0.8203210526],
-         'Left_Corticospinal_surf.vtk': [0.08428421053,0.3471947368,0.8573315789],
-         'Left_IFOF_surf.vtk':[0.2652894737,0.7327157895,0.5916368421],
-         'Left_ILF_surf.vtk':[0.5624210526,0.7486894737,0.4528842105],
-         'Left_SLF_surf.vtk':[0.7996473684,0.7343736842,0.3576473684],
-         'Left_Thalamic_Radiation_surf.vtk': [0.2081,0.1663,0.5292],
-         'Left_Uncinate_surf.vtk':[0.9944105263,0.7464157895,0.2389842105],
-         'Right_Arcuate_surf.vtk':[0.9763,0.9831,0.0538], 
-         'Right_Cingulum_Cingulate_surf.vtk': [0.07765263158,0.5299631579,0.8278947368], 
-         'Right_Cingulum_Hippocampus_surf.vtk':[0.02300526316,0.6443421053,0.7882842105],
-         'Right_Corticospinal_surf.vtk': [0.01574736842,0.4257052632,0.8789315789],
-         'Right_IFOF_surf.vtk':[0.4175631579,0.7470789474,0.5142157895],
-         'Right_ILF_surf.vtk':[0.6871684211,0.7432631579,0.4029421053],
-         'Right_SLF_surf.vtk':[0.9056631579,0.7261052632,0.3104684211],
-         'Right_Thalamic_Radiation_surf.vtk': [0.2052473684,0.2466526316,0.6930631579],
-         'Right_Uncinate_surf.vtk':[0.9847368421,0.8141052632,0.1733684211]}
+#color = {'Callosum_Forceps_Major_surf.vtk':[0.04850526316,0.6792842105,0.7341421053],
+#         'Callosum_Forceps_Minor_surf.vtk':[0.1400526316,0.7084789474,0.6680368421],
+ #        'Left_Arcuate_surf.vtk':[0.9595947368,0.8869315789,0.1189526316], 
+ #        'Left_Cingulum_Cingulate_surf.vtk': [0.06577894737,0.4776315789,0.8531631579], 
+ #        'Left_Cingulum_Hippocampus_surf.vtk':[0.03562105263,0.5946210526,0.8203210526],
+ #        'Left_Corticospinal_surf.vtk': [0.08428421053,0.3471947368,0.8573315789],
+ #        'Left_IFOF_surf.vtk':[0.2652894737,0.7327157895,0.5916368421],
+ #        'Left_ILF_surf.vtk':[0.5624210526,0.7486894737,0.4528842105],
+ #        'Left_SLF_surf.vtk':[0.7996473684,0.7343736842,0.3576473684],
+ #        'Left_Thalamic_Radiation_surf.vtk': [0.2081,0.1663,0.5292],
+ #        'Left_Uncinate_surf.vtk':[0.9944105263,0.7464157895,0.2389842105],
+ #        'Right_Arcuate_surf.vtk':[0.9763,0.9831,0.0538], 
+ #        'Right_Cingulum_Cingulate_surf.vtk': [0.07765263158,0.5299631579,0.8278947368], 
+ #        'Right_Cingulum_Hippocampus_surf.vtk':[0.02300526316,0.6443421053,0.7882842105],
+ #        'Right_Corticospinal_surf.vtk': [0.01574736842,0.4257052632,0.8789315789],
+ #        'Right_IFOF_surf.vtk':[0.4175631579,0.7470789474,0.5142157895],
+ #        'Right_ILF_surf.vtk':[0.6871684211,0.7432631579,0.4029421053],
+ #        'Right_SLF_surf.vtk':[0.9056631579,0.7261052632,0.3104684211],
+ #        'Right_Thalamic_Radiation_surf.vtk': [0.2052473684,0.2466526316,0.6930631579],
+ #        'Right_Uncinate_surf.vtk':[0.9847368421,0.8141052632,0.1733684211]}
 
 
 
