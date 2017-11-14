7376bfc1bb4d1fc2e1eebc630a7e8ca001949cff
kitchell <lindseykitchell21@gmail.com>
2017-11-14 20:28:14 +0000

    make exec

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ name=plot3dsurfaces
 docker build . -t $name && \
 docker run \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /mnt/scratch/kitchell/sigularitytest:/output \
+    -v /mnt/scratch/kitchell/singularitytest:/output \
     --privileged -t --rm \
     singularityware/docker2singularity \
     $name
