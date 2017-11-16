15817e6d8aa8d3eb80a8d86f6c0e9566dcee82b5
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-11-15 21:28:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,8 @@ json_file = {}
 file_list = []
 for file in glob.glob(config["surfaces"] + "/*.vtk"):
     #print file
-    fname = os.path.basename(file)[0:-9]
+    fname = os.path.basename(file)[0:-4]
+    print fname
     if fname in color.keys():
 #for file in glob.glob('surfaces/*.vtk'):
         for d in range(len(camera_pos)):
